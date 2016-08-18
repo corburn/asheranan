086d186c4ed4572c7431853821850b6055ae18a3
Jason Travis <JasonTravis@nau.edu>
2016-08-18 22:51:43 +0000

    Reenable make target directories

--- samtools.mk
+++ samtools.mk
@@ -69,7 +69,7 @@ $(addprefix ${SRC}/bcftools/, 1.0 1.1):
 $(addprefix ${LOCAL}/samtools/, $(SAMTOOLS_V0_1_X)): ${LOCAL}/samtools/%: ${SRC}/samtools/%
 	sed -i "s|^prefix.*=.*|prefix = $@|" $</Makefile
 	$(MAKE) -C $<
-	#mkdir -m 755 -p $@/bin $@/share/man1/man
+	mkdir -m 755 -p $@/bin $@/share/man1/man
 	install -D --target-directory=$@/bin $(addprefix $</, samtools bcftools/bcftools bcftools/vcfutils.pl)
 	install -D --target-directory=$@/bin $(filter-out $</misc/Makefile $(wildcard $</misc/*.h) $(wildcard $</misc/*.c) $(wildcard $</misc/*.java), $(wildcard $</misc/*))
 	install -D --target-directory=$@/share/man --mode=0644 $</samtools.1 || true
